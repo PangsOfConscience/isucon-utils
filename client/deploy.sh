5fe6b0a4c31f0fbff157358e9aa02fd0647016a0
Kenji Shimizu <jrywm121@gmail.com>
2021-05-15 00:12:36 +0900

    クライアントからキックするデプロイスクリプト

---
+++ client/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# サーバ上にあるデプロイスクリプトをキックする
+
+set -eu
+
+echo_err() {
+    echo "$1" 1>&2
+}
+
+main() {
+    local -r script_dir=$(
+        cd $(dirname $0)
+        pwd
+    )
+
+    # 参考: https://blog.yuuk.io/entry/web-operations-isucon
+    local -r server=$1
+    local -r user=$USER
+    ssh -F ${script_dir}/.sshconfig $server "/home/isucon/deploy.sh"
+}
+
+main "$@"
