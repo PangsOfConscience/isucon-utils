ea841143e4d5bc10dd5e6850b25c18f237068da6
Kenji Shimizu <jrywm121@gmail.com>
2021-07-18 22:28:15 +0900

    🐛 fix permission

--- server/analyze.sh
+++ server/analyze.sh
@@ -25,8 +25,8 @@ main() {
     EIP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
     ${script_dir}/notify.sh "Analysis done! Please check http://${EIP}:9091/${alp_file_name}.html and http://${EIP}:9091/${pqd_file_name}.html"
 
-    mv /var/log/nginx/access.log /var/log/nginx/access_${current}.log
-    mv /var/log/mysql/slow.log /var/log/mysql/slow_${current}.log
+    sudo mv /var/log/nginx/access.log /var/log/nginx/access_${current}.log
+    sudo mv /var/log/mysql/slow.log /var/log/mysql/slow_${current}.log
 }
 
 main "$@"
