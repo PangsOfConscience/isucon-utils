da7bd562910eeb998bc50adfe291ac4d8a5562c1
Kenji Shimizu <jrywm121@gmail.com>
2021-07-18 22:25:09 +0900

    ✨ add feature about pt-query-digest

--- server/analyze.sh
+++ server/analyze.sh
@@ -10,18 +10,23 @@ main() {
     )
     current=$(date +"%H-%M-%S")
     alp_file_name="alp-${user}-${current}"
+    pqd_file_name="pt-query-digest-${user}-${current}"
 
     sudo mkdir -p /www/data
     sudo chmod 777 /www/data
     sudo chmod 777 /var/log/nginx/access.log
+    sudo chmod 777 /var/log/mysql && sudo chmod 777 /var/log/mysql/slow.log
     echo "<html><body><pre style=\"font-family: 'Courier New', Consolas\">" >/www/data/${alp_file_name}.html
     cat /var/log/nginx/access.log | alp ltsv ${alp_command_opts} >>/www/data/${alp_file_name}.html
+    echo "<html><body><pre style=\"font-family: 'Courier New', Consolas\">" >/www/data/${pqd_file_name}.html
+    pt-query-digest /var/log/mysql/slow.log >>/www/data/${pqd_file_name}.html
 
     TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
     EIP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
-    ${script_dir}/notify.sh "Analysis done! Please check http://${EIP}:9091/${alp_file_name}.html"
+    ${script_dir}/notify.sh "Analysis done! Please check http://${EIP}:9091/${alp_file_name}.html and http://${EIP}:9091/${pqd_file_name}.html"
 
     mv /var/log/nginx/access.log /var/log/nginx/access_${current}.log
+    mv /var/log/mysql/slow.log /var/log/mysql/slow_${current}.log
 }
 
 main "$@"
